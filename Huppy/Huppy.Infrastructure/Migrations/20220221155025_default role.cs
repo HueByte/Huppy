cea26d073893b7de20a6c22f86825cd95331e828
HueByte <ainodeve@gmail.com>
2022-02-21 16:52:27 +0100

    Reconstructed server repository

---
+++ Huppy/Huppy.Infrastructure/Migrations/20220221155025_default role.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Huppy.Infrastructure.Migrations
+{
+    public partial class defaultrole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<ulong>(
+                name: "RoleID",
+                table: "Servers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0ul);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "RoleID",
+                table: "Servers");
+        }
+    }
+}
--- Huppy/Huppy.Infrastructure/Repositories/ServerRepository.cs
+++ Huppy/Huppy.Infrastructure/Repositories/ServerRepository.cs
@@ -1,3 +1,4 @@
+using Discord.Interactions;
 using Huppy.Core.IRepositories;
 using Huppy.Core.Models;
 using Microsoft.EntityFrameworkCore;
@@ -16,6 +17,52 @@ namespace Huppy.Infrastructure.Repositories
         {
             await _context.Servers.AddAsync(server);
             await _context.SaveChangesAsync();
+        }
+
+        public async Task AddOneAsync(ShardedInteractionContext DiscordContext)
+        {
+            var server = await _context.Servers.FirstOrDefaultAsync(en => en.ID == DiscordContext.Guild.Id);
+            if (server is not null)
+            {
+                throw new Exception($"Server with [{DiscordContext.Guild.Id}] ID already exists");
+            }
+
+            server = new()
+            {
+                ID = DiscordContext.Guild.Id,
+                GreetMessage = "",
+                OutputRoom = DiscordContext.Guild.DefaultChannel.Id,
+                UseGreet = false,
+                ServerName = DiscordContext.Guild.Name,
+                RoleID = 0
+            };
+
+            await _context.Servers.AddAsync(server);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<Server> GetOrCreateAsync(ShardedInteractionContext DiscordContext)
+        {
+            var server = await _context.Servers.FirstOrDefaultAsync(en => en.ID == DiscordContext.Guild.Id);
+            if (server is not null)
+            {
+                return server;
+            }
+
+            server = new()
+            {
+                ID = DiscordContext.Guild.Id,
+                GreetMessage = "",
+                OutputRoom = DiscordContext.Guild.DefaultChannel.Id,
+                UseGreet = false,
+                ServerName = DiscordContext.Guild.Name,
+                RoleID = 0
+            };
+
+            await _context.Servers.AddAsync(server);
+            await _context.SaveChangesAsync();
+
+            return server;
         }
 
         public async Task<Server?> GetOneAsync(ulong ID)
