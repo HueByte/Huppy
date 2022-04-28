e7a0225faaef023f830c9752681fb4565db9fbd0
HueByte <ainodeve@gmail.com>
2022-04-28 23:39:50 +0200

    Updated EventService interface

--- Huppy/Huppy.Core/Services/EventService/IEventService.cs
+++ Huppy/Huppy.Core/Services/EventService/IEventService.cs
@@ -2,6 +2,9 @@ namespace Huppy.Core.Services.EventService
 {
     public interface IEventService
     {
-         
+        void AddEvent(DateTime time, Func<Task> action);
+        void AddEvent(DateTime time, Task action);
+        void AddRange(DateTime time, List<Func<Task>> actions);
+        
     }
 }
