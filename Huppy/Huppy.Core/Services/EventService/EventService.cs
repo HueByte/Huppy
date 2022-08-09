9f59d50b035e12f43a2035ad0962dd4083c6d293
HueByte <ainodeve@gmail.com>
2022-08-09 21:18:32 +0200

    Removed unused methods from EventService

--- Huppy/Huppy.Core/Services/EventService/EventService.cs
+++ Huppy/Huppy.Core/Services/EventService/EventService.cs
@@ -7,11 +7,9 @@ namespace Huppy.Core.Services.EventService
     public class EventService : IEventService
     {
         private readonly ILogger _logger;
-        public readonly Dictionary<ulong, List<TimedEvent>> events = new();
         public readonly ConcurrentDictionary<ulong, List<Event>> jobs = new();
         public event Func<string[], Task> OnEventsRemoved;
         private readonly SemaphoreSlim _semiphore = new(4);
-        private readonly object _lockObj = new();
         private Timer? _timer;
         private DateTime _startDate;
         private readonly TimeSpan _ticker = TimeSpan.FromSeconds(1); // ticks
@@ -60,121 +58,67 @@ namespace Huppy.Core.Services.EventService
             _semiphore.Release();
         }
 
-        // OLD
-        public void AddEvent(DateTime time, TimedEvent action)
-        {
-            AddRange(time, new List<TimedEvent> { action });
-        }
+        // To remake
+        // public Task RemovePrecise(DateTime? time, string name)
+        // {
+        //     if (string.IsNullOrEmpty(name) || time is null) return Task.CompletedTask;
 
-        public void AddEvent(DateTime time, string eventName, Func<Task> action)
-        {
-            if (string.IsNullOrEmpty(eventName)) return;
+        //     var targetTime = GetTargetTime((DateTime)time);
+        //     if (events.ContainsKey(targetTime))
+        //     {
+        //         events.TryGetValue(targetTime, out var eventList);
+        //         if (eventList is null)
+        //         {
+        //             _logger.LogError("Precise remove error {time} <{name}>: event list was null", time, name);
+        //             return Task.CompletedTask;
+        //         }
 
-            List<TimedEvent> newEvents = new()
-            {
-                new TimedEvent {
-                    Name = eventName,
-                    Event = action
-                }
-            };
+        //         var eventToRemove = eventList.FirstOrDefault(e => e.Name == name);
+        //         if (eventToRemove is null)
+        //         {
+        //             _logger.LogError("Precise remove error {time} <{name}>: didn't find event with provided name", time, name);
+        //             return Task.CompletedTask;
+        //         }
 
-            AddRange(time, newEvents);
-        }
+        //         eventList.Remove(eventToRemove);
 
-        public void AddEvent(DateTime time, string eventName, Task action)
-        {
-            if (string.IsNullOrEmpty(eventName)) return;
+        //         lock (_lockObj)
+        //         {
+        //             events[targetTime] = eventList;
+        //         }
+        //     }
 
-            List<TimedEvent> newEvents = new()
-            {
-                new TimedEvent {
-                    Name = eventName,
-                    Event = async () => await action
-                }
-            };
+        //     return Task.CompletedTask;
+        // }
 
-            AddRange(time, newEvents);
-        }
+        // public Task RemoveEventsByName(string name)
+        // {
+        //     if (string.IsNullOrEmpty(name)) return Task.CompletedTask;
 
-        public void AddRange(DateTime time, List<TimedEvent> actions)
-        {
-            var targetTime = GetTargetTime(time);
-            lock (_lockObj)
-            {
-                if (events.ContainsKey(targetTime))
-                {
-                    events.TryGetValue(targetTime, out var value);
-                    value ??= new();
-                    value.AddRange(actions);
-                }
+        //     var eventsToRemove = events.Where(e => e.Value.Any(e => e.Name == name));
 
-                if (events.TryAdd(targetTime, actions))
-                {
-                    _logger.LogDebug("Added {count} events {date} UTC", actions.Count, time);
-                }
-            }
-        }
+        //     foreach (var time in eventsToRemove)
+        //     {
+        //         events.Remove(time.Key);
+        //     }
 
-        public Task RemovePrecise(DateTime? time, string name)
-        {
-            if (string.IsNullOrEmpty(name) || time is null) return Task.CompletedTask;
+        //     return Task.CompletedTask;
+        // }
 
-            var targetTime = GetTargetTime((DateTime)time);
-            if (events.ContainsKey(targetTime))
-            {
-                events.TryGetValue(targetTime, out var eventList);
-                if (eventList is null)
-                {
-                    _logger.LogError("Precise remove error {time} <{name}>: event list was null", time, name);
-                    return Task.CompletedTask;
-                }
+        // public Task RemoveEventsByTime(DateTime time)
+        // {
+        //     var targetTime = GetTargetTime(time);
+        //     if (events.ContainsKey(targetTime))
+        //     {
+        //         lock (_lockObj)
+        //         {
+        //             if (!events.Remove(targetTime))
+        //                 _logger.LogError("Failed to removed events at {time}", targetTime);
+        //         }
+        //     }
 
-                var eventToRemove = eventList.FirstOrDefault(e => e.Name == name);
-                if (eventToRemove is null)
-                {
-                    _logger.LogError("Precise remove error {time} <{name}>: didn't find event with provided name", time, name);
-                    return Task.CompletedTask;
-                }
-
-                eventList.Remove(eventToRemove);
-
-                lock (_lockObj)
-                {
-                    events[targetTime] = eventList;
-                }
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public Task RemoveEventsByName(string name)
-        {
-            if (string.IsNullOrEmpty(name)) return Task.CompletedTask;
-
-            var eventsToRemove = events.Where(e => e.Value.Any(e => e.Name == name));
-
-            foreach (var time in eventsToRemove)
-            {
-                events.Remove(time.Key);
-            }
-
-            return Task.CompletedTask;
-        }
-
-        public Task RemoveEventsByTime(DateTime time)
-        {
-            var targetTime = GetTargetTime(time);
-            if (events.ContainsKey(targetTime))
-            {
-                lock (_lockObj)
-                {
-                    if (!events.Remove(targetTime))
-                        _logger.LogError("Failed to removed events at {time}", targetTime);
-                }
-            }
-
-            return Task.CompletedTask;
-        }
+        //     return Task.CompletedTask;
+        // }
 
         public DateTime GetStartTime() => _startDate;
 
@@ -186,43 +130,6 @@ namespace Huppy.Core.Services.EventService
             }, null, new TimeSpan(0), _ticker);
 
             return timer;
-        }
-
-        private Task ExecuteEvents()
-        {
-            try
-            {
-                var targetTime = GetTargetTime(DateTime.UtcNow);
-
-                // TODO: find better solution
-                var executionEvents = events.Where(e => e.Key < targetTime);
-
-                if (executionEvents.Any())
-                {
-                    foreach (var tasks in executionEvents)
-                    {
-                        foreach (var task in tasks.Value)
-                        {
-                            // fire and forget
-                            _ = Task.Run(task.Event);
-                        }
-                    }
-                }
-
-                foreach (var key in executionEvents)
-                {
-                    lock (_lockObj)
-                    {
-                        events.Remove(key.Key);
-                    }
-                }
-            }
-            catch (Exception e)
-            {
-                _logger.LogError("{message} - {stackTrace}", e.Message, e.StackTrace);
-            }
-
-            return Task.CompletedTask;
         }
 
         private async Task Execute()
