52f82f1690a4208acc212bf3eb04ccdfb152e5dd
HueByte <ainodeve@gmail.com>
2022-08-12 17:29:55 +0200

    Fixed purge command

--- Huppy/Huppy.App/Commands/AdminCommands.cs
+++ Huppy/Huppy.App/Commands/AdminCommands.cs
@@ -23,12 +23,29 @@ namespace Huppy.App.Commands
         [EnabledInDm(true)]
         public async Task PurgeMessages(int count)
         {
+            if (count <= 0)
+            {
+                await ModifyOriginalResponseAsync((msg) =>
+                {
+                    msg.Embed = new EmbedBuilder()
+                        .WithTitle("Purge Report")
+                        .WithThumbnailUrl(Icons.Huppy1)
+                        .WithAuthor(Context.User)
+                        .WithColor(Color.LightOrange)
+                        .WithDescription("Count number must be higher than 0")
+                        .Build();
+                });
+
+                return;
+            }
+
             var messages = await Context.Channel.GetMessagesAsync(Context.Interaction.Id, Direction.Before, count).FlattenAsync();
 
             // check if message is ephemeral & date of the message is not 14 days away
             var filteredMessages = messages.Where(msg => msg.Flags != MessageFlags.Ephemeral && (DateTimeOffset.UtcNow - msg.Timestamp).TotalDays <= 14);
+            int filteredMessagesCount = filteredMessages.Count();
 
-            _logger.LogInformation("Trying to remove {count} of messages", filteredMessages.Count());
+            _logger.LogInformation("Trying to remove {count} of messages", filteredMessagesCount);
 
             await ((ITextChannel)Context.Channel).DeleteMessagesAsync(filteredMessages);
 
@@ -37,21 +54,21 @@ namespace Huppy.App.Commands
                 .WithAuthor(Context.User)
                 .WithColor(Color.LightOrange);
 
-            if (count != filteredMessages.Count())
+            if (count != filteredMessagesCount)
                 embed.WithDescription("*Your input message and result message count might be different as bots cannot remove ephemeral messages*");
 
-            var firstMessagesNumber = count > 10 ? 10 : count;
+            var previewMessagesNumber = filteredMessagesCount > 10 ? 10 : filteredMessagesCount;
 
-            var firstMessages = filteredMessages
-                .Take(firstMessagesNumber)
+            var previewMessages = filteredMessages
+                .Take(previewMessagesNumber)
                 .Select(msg => new string($"> **{msg.Author}**# *{(!string.IsNullOrEmpty(msg.Content) ? msg.Content : "No Text")}*"));
 
             embed.AddField("Input number", $"`{count}`", true);
-            embed.AddField("Messages removed", $"`{filteredMessages.Count()}`", true);
+            embed.AddField("Messages removed", $"`{filteredMessagesCount}`", true);
             embed.AddField("Date of removal", TimestampTag.FromDateTime(DateTime.UtcNow), true);
 
-            if (firstMessages.Any())
-                embed.AddField($"First {firstMessagesNumber} messages removed", string.Join("\n\n", firstMessages));
+            if (previewMessages.Any())
+                embed.AddField($"First {(previewMessagesNumber > 1 ? $"`{previewMessagesNumber}` messages" : "one message")} removed", string.Join("\n\n", previewMessages));
 
             await ModifyOriginalResponseAsync((msg) => msg.Embed = embed.Build());
         }
