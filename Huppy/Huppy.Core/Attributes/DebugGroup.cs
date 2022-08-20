f2927381ce19bee08655dfe71fbc9838b94c4eb4
HueByte <ainodeve@gmail.com>
2022-08-21 01:45:07 +0200

    Added new attributes for further permission development

--- Huppy/Huppy.Core/Attributes/DebugGroup.cs
+++ Huppy/Huppy.Core/Attributes/DebugGroup.cs
@@ -1,8 +1,10 @@
 using System.Diagnostics;
+using Discord.Interactions;
 
 namespace Huppy.Core.Attributes
 {
     [System.AttributeUsage(System.AttributeTargets.Class, AllowMultiple = true)]
+    [DontAutoRegister]
     public class DebugGroupAttribute : Attribute
     {
         public DebugGroupAttribute() { }
