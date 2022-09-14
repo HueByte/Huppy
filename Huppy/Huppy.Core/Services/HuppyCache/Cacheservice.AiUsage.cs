720f351c654434637bd976ad8c8e51a5515153ad
LeeMustache <31374466+LeeMustache@users.noreply.github.com>
2022-09-14 16:54:38 +0200

    Fix formatting

--- Huppy/Huppy.Core/Services/HuppyCache/Cacheservice.AiUsage.cs
+++ Huppy/Huppy.Core/Services/HuppyCache/Cacheservice.AiUsage.cs
@@ -11,30 +11,30 @@ public partial class CacheService
 
     public Task<AiUser> GetUserUsage(ulong UserId)
     {
-        _userAiUsage .TryGetValue(UserId, out var user);
+        _userAiUsage.TryGetValue(UserId, out var user);
         return Task.FromResult(user!);
     }
 
     public Task<int> GetCurrentMessageCount()
     {
-        var count = _userAiUsage .Sum(e => e.Value.Count);
+        var count = _userAiUsage.Sum(e => e.Value.Count);
         return Task.FromResult(count);
     }
 
     public Task<Dictionary<ulong, AiUser>> GetAiStatistics()
     {
-        return Task.FromResult(_userAiUsage .ToDictionary(p => p.Key, p => p.Value));
+        return Task.FromResult(_userAiUsage.ToDictionary(p => p.Key, p => p.Value));
     }
 
     private Task AddToCache(ulong UserId, string Username)
     {
-        if (_userAiUsage .TryGetValue(UserId, out var currentValue))
+        if (_userAiUsage.TryGetValue(UserId, out var currentValue))
         {
-            _userAiUsage [UserId].Count = currentValue.Count + 1;
+            _userAiUsage[UserId].Count = currentValue.Count + 1;
         }
         else
         {
-            _userAiUsage .TryAdd(UserId, new AiUser { Username = Username, Count = 1 });
+            _userAiUsage.TryAdd(UserId, new AiUser { Username = Username, Count = 1 });
         }
 
         return Task.CompletedTask;
