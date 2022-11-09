96059692d0392d98e57ea97fb2286c5d9fcd87e6
huebyte <ihuebyte@gmail.com>
2022-11-09 03:09:38 +0100

    T #98 Mapper fixes

--- src/HuppyService/HuppyService.Service/Configuration/MappingProfile.cs
+++ src/HuppyService/HuppyService.Service/Configuration/MappingProfile.cs
@@ -9,19 +9,30 @@ namespace HuppyService.Service.Configuration
     {
         public MappingProfile()
         {
-            CreateMap<CommandLogModel, CommandLog>()
-                .ForMember(dest => dest.Date,
-                    opt => opt.MapFrom(src => Miscellaneous.UnixTimeStampToUtcDateTime(src.Date)));
-
             CreateMap<CommandLog, CommandLogModel>()
+                .ForSourceMember(dest => dest.Guild,
+                    opt => opt.DoNotValidate())
                 .ForMember(dest => dest.Date,
                     opt => opt.MapFrom(src => Miscellaneous.DateTimeToUnixTimeStamp(src.Date)));
 
-            CreateMap<ServerModel, Server>()
-                .ReverseMap();
+            CreateMap<CommandLogModel, CommandLog>()
+                .ForMember(dest => dest.Guild,
+                    opt => opt.Ignore())
+                .ForMember(dest => dest.Date,
+                    opt => opt.MapFrom(src => Miscellaneous.UnixTimeStampToUtcDateTime(src.Date)));
 
             CreateMap<ServerRoomsModel, ServerRooms>()
+                .ForMember(dest => dest.Server,
+                    opt => opt.Ignore())
                 .ReverseMap();
+
+            CreateMap<ServerModel, Server>()
+                .ForMember(dest => dest.CommangLogs,
+                    opt => opt.Ignore());
+
+            CreateMap<Server, ServerModel>()
+                .ForSourceMember(dest => dest.CommangLogs,
+                    opt => opt.DoNotValidate());
 
             CreateMap<ReminderModel, Reminder>()
                 .ForMember(dest => dest.RemindDate,
